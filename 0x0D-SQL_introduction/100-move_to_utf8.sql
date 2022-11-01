e791df16fd683628c77899f1daa04ff24587eab2
luifesinachi <luckyogana@gmail.com>
2022-11-01 14:48:31 -0700

    updated

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,7 @@
 -- Database hbtn_0c_0
 -- Table first_table
 -- Field name in first_table
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY `name` varchar(256) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;
