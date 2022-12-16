c12726f695e4d650a838915d12cdbd916f9447b8
luifesinachi <luckyogana@gmail.com>
2022-12-16 12:14:12 -0800

    my first commit

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Program that fetch an URL with requests package """
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
