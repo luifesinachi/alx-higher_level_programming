24793751f42ba31111adce8cb9eb5f495c22869e
luifesinachi <luckyogana@gmail.com>
2022-12-16 19:51:11 -0800

    updated

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,14 +5,12 @@ const dict = require('./101-data').dict;
 const newDictOccurence = {};
 
 for (const [key, value] of Object.entries(dict)) {
-	  if (newDictOccurence[value]) 
+	if (newDictOccurence[value]) 
 	{
 		  newDictOccurence[value].push(key)
+	  } else {
+		  (newDictOccurence[value] = [key]);
 	  }
-	else 
-	{
-			    (newDictOccurence[value] = [key]);
-		    }
 }
 
 console.log(newDictOccurence);
